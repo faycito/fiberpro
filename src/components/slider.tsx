e6369499ba5f7a9e5c713d9105ac202ab7ed78fa
Ismael Alexander Malca Castro <ismael.malca@caylent.com>
2024-03-07 16:14:01 -0500

    fix mobile ux

--- src/components/slider.tsx
+++ src/components/slider.tsx
@@ -22,7 +22,7 @@ const settings = {
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 4,
+          slidesToShow: 3,
           slidesToScroll: 1,
         }
       },
