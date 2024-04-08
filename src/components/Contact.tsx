79091ddc031733cad1c7604e55d321fd4370020d
Ismael Alexander Malca Castro <ismael.malca@caylent.com>
2024-04-08 11:12:33 -0500

    successssss mege added

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -12,6 +12,7 @@ const Contact = () => {
 	const [phone, setPhone] = useState('');
 	const [docNum, setDocNum] = useState('');
 	const [checked, setChecked] = useState(false);
+	const [success, setSuccess] = useState('');
 
 	const onChangeInput = (name: string, event: React.ChangeEvent<HTMLInputElement>) => {
 		if(name === 'documento') setDocNum(event.target.value);
@@ -28,17 +29,23 @@ const Contact = () => {
 				name: docNum,
 				phone,
 				provider: 'LandingPage Pixelio',
-				campaign: 'Testing'
+				campaing: 'Testing'
 			}
 
-			const response = await fetch('http://201.218.159.60:5000/api/leads', {
+			const response = await fetch('http://192.168.31.78:3001/api/leads', {
 				body: JSON.stringify(body),
-				method: 'POST'
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+					'Accept': '*/*'
+				}
 			});
 	
 			const data = await response.json();
-			console.log(data);
+			console.log(data.message)
+			setSuccess(data.message);
 		} catch (error) {
+			console.log("ERROR")
 			console.log(error);			
 		}
 	}
@@ -52,51 +59,56 @@ const Contact = () => {
 				fontSize: 18
 			}}
 		>¡Déjanos tus datos y te llamamos!</p>
-		<form onSubmit={onSubmit}>
-		<input 
-			type="text"
-			value={docNum}
-			name="documento"
-			placeholder="DNI/CEX"
-			onChange={(ev) => onChangeInput('documento', ev)}
-			className="text-center border-2 border-primary mt-4 mb-1 text-sm py-2 rounded-xl w-full"
-		/>
-		<input 
-			type="tel"
-			value={phone}
-			name="telefono" 
-			placeholder="Teléfono" 
-			onChange={(ev) => onChangeInput('telefono', ev)}
-			className="text-center border-2 border-primary my-2 text-sm py-2 rounded-xl w-full" 
-		/>
-		<Checkbox
-			name="check"
-			checked={checked}
-			icon={<div>
-				<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="15" height="15" viewBox="0 0 48 48">
-					<path fill="#FFF" stroke='2px' d="M40.6 12.1L17 35.7 7.4 26.1 4.6 29 17 41.3 43.4 14.9z"></path>
-				</svg>
-			</div>}
-			onChange={(val: boolean) => setChecked(val)}
-			label={'Acepto los términos y condiciones'}
-			size={20}
-			borderRadius={20}
-			style={{
-				backgroundColor: checked ? '#007aff'  : ''
-			}}
-			borderColor={checked ? '#00000000':'#bababa'}
-			labelStyle={{fontSize: 10, marginLeft: 4, color: '#EB5612'}}
-		/>
-		<button
-			className='px-6 py-2 bg-primary text-white rounded-full font-medium mt-4 disabled:opacity-50'
-			disabled={
-				!(phone.length > 6 &&
-				docNum.length >= 8 && docNum.length <= 13 &&
-				checked)
-			}
-			type={'submit'}
-		>Llámenme</button>
-		</form>
+		{ !success ? (
+			<form onSubmit={onSubmit}>
+			<input 
+				type="text"
+				value={docNum}
+				name="documento"
+				placeholder="DNI/CEX"
+				onChange={(ev) => onChangeInput('documento', ev)}
+				className="text-center border-2 border-primary mt-4 mb-1 text-sm py-2 rounded-xl w-full"
+			/>
+			<input 
+				type="tel"
+				value={phone}
+				name="telefono" 
+				placeholder="Teléfono" 
+				onChange={(ev) => onChangeInput('telefono', ev)}
+				className="text-center border-2 border-primary my-2 text-sm py-2 rounded-xl w-full" 
+			/>
+			<Checkbox
+				name="check"
+				checked={checked}
+				icon={<div>
+					<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="15" height="15" viewBox="0 0 48 48">
+						<path fill="#FFF" stroke='2px' d="M40.6 12.1L17 35.7 7.4 26.1 4.6 29 17 41.3 43.4 14.9z"></path>
+					</svg>
+				</div>}
+				onChange={(val: boolean) => setChecked(val)}
+				label={'Acepto los términos y condiciones'}
+				size={20}
+				borderRadius={20}
+				style={{
+					backgroundColor: checked ? '#007aff'  : ''
+				}}
+				borderColor={checked ? '#00000000':'#bababa'}
+				labelStyle={{fontSize: 10, marginLeft: 4, color: '#EB5612'}}
+			/>
+			<button
+				className='px-6 py-2 bg-primary text-white rounded-full font-medium mt-4 disabled:opacity-50'
+				disabled={
+					!(phone.length > 6 &&
+					docNum.length >= 8 && docNum.length <= 13 &&
+					checked)
+				}
+				type={'submit'}
+			>Llámenme</button>
+			</form>
+		) : (
+			<h2 className='text-primary font-normal mt-[30%]'>{success}</h2>
+		)
+		}
   	</div>
   )
 }
